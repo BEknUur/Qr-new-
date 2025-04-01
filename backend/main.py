1714a9d03a0b31dc3c7dafa0f5270c2c797e0fd9
Beknur <106554639+BEknUur@users.noreply.github.com>
2025-04-01 14:49:46 +0500

    photos

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from app.core.config import settings
 import os
 from app.core.database import Base, engine
-from app.routes import auth_routes, profile_routes, chat_routes, car_routes,chat_websocket
+from app.routes import auth_routes, profile_routes, chat_routes, car_routes, chat_websocket
 from app.routes.favorite_routes import router as favorite_router
 
 load_dotenv()
@@ -22,14 +22,16 @@ app.add_middleware(
 )
 
 
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-app.mount("/car_uploads", StaticFiles(directory="car_uploads"), name="car_uploads")
-app.mount("/uploaded_images", StaticFiles(directory="uploaded_images"), name="uploaded_images")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app.mount("/uploads", StaticFiles(directory=os.path.join(BASE_DIR, "uploads")), name="uploads")
+app.mount("/car_uploads", StaticFiles(directory=os.path.join(BASE_DIR, "car_uploads")), name="car_uploads")
+app.mount("/uploaded_images", StaticFiles(directory=os.path.join(BASE_DIR, "uploaded_images")), name="uploaded_images")
 
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
 app.include_router(profile_routes.router, prefix="/profile", tags=["profile"])
-app.include_router(chat_routes.router, prefix="/chat", tags=["chat"])  
+app.include_router(chat_routes.router, prefix="/chat", tags=["chat"])
 app.include_router(car_routes.router, prefix="/car", tags=["car"])
-app.include_router(chat_websocket.router)  
+app.include_router(chat_websocket.router)
 app.include_router(favorite_router)
